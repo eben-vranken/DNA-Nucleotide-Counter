c3cd8084f8a4c01ef8b5372c972936d3bc681d63
eben-vranken <vranken.eben@outlook.com>
2026-07-03 15:47:13 +0200

    feat: sanitize nucleotide output based upon analysis mode

--- cli.py
+++ cli.py
@@ -6,8 +6,8 @@ def read_args():
     parser.add_argument("file", help="Data file to read")
 
     # Optional arguments
-    parser.add_argument("--mode", help="Analysis mode (dna/rna)")
-    parser.add_argument("--format", help="Output format (json/csv/txt)")
+    parser.add_argument("--mode", choices=["dna", "rna"], default="dna", help="Analysis mode (dna/rna)")
+    parser.add_argument("--format", choices=["json", "csv", "txt"], default="json", help="Output format (json/csv/txt)")
     parser.add_argument("--output", help="Output file path")
 
     return parser.parse_args()
@@ -16,4 +16,10 @@ if  __name__ == "__main__":
     args = read_args()
     nucleic_acid_string = parser.read_file(args.file)
     nucleotide_count = parser.count_nucleotides(nucleic_acid_string)
+    
+    if args.mode == "dna":
+        nucleotide_count.pop("U", None)
+    else:
+        nucleotide_count.pop("T", None)
+
     print(nucleotide_count)
